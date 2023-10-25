31310c4fe7c74edfccac274301301638f1062f34
David <david.sanchez@tecnologicoliceocristiano.edu.ec>
2023-10-24 21:09:10 -0500

    creamos variable, y también el BINDING

--- toDoListApp/MainPage.xaml.cs
+++ toDoListApp/MainPage.xaml.cs
@@ -1,14 +1,23 @@
-﻿namespace toDoListApp
+﻿using System.Collections.ObjectModel;
+
+namespace toDoListApp
 {
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        public ObservableCollection<Task> MyTasks { get; set; }
+        public string MyTitle { get; set; } = "Lista de Tareas";
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-       
+        private void AddBtn_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("¿Seguro que quieres salir?",
+                "Al salir tu riñon izquierdo será nuestro",
+                "Bueno....");
+        }
     }
 }
